c29623d7413d6e2bb41a65855fcdde6cac60cdb5
shekh_khwaja <khwajajulaha122@gmal.com>
2025-11-02 08:56:00 +0530

    today

---
+++ Basic/myfirstprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>//#include <stdio.h> is a header file library that lets us work with input and output functions, such as printf() (used in line 4). Header files add functionality to C programs.
+int main()//Another thing that always appear in a C program is main(). This is called a function. Any code inside its curly brackets {} will be executed.
+{
+    printf("hello world");// for printing output 
+    return(0);
+}
+
+
+/*  Note that: Every C statement ends with a semicolon ;
+
+Note: The body of int main() could also been written as:
+int main(){printf("Hello World!");return 0;}
+
+*/
---
+++ Basic/newline.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");// => \n for starting new line in c  
+  printf("I am learning C.");
+  return 0;
+}
+////
